e3224ee08efb2207e17125f6f9c6a899df8e8c06
Lucas Teixeira <lucastex@gmail.com>
2009-08-12 09:53:57 -0300

    reordering packages

--- grails-app/conf/Config.groovy
+++
@@ -1,4 +0,0 @@
-import br.com.caelum.grails.stella.CPFConstraint
-import org.codehaus.groovy.grails.validation.ConstrainedProperty
-
-ConstrainedProperty.registerNewConstraint(CPFConstraint.CPF_CONSTRAINT, CPFConstraint.class)
