c4b15189ec2c963c6285e7ad9b0efae13c1f42e2
Lucas Teixeira <lucastex@gmail.com>
2009-08-13 17:56:48 -0300

    changed version to 1.1

--- GrailsStellaGrailsPlugin.groovy
+++ GrailsStellaGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import org.codehaus.groovy.grails.validation.ConstrainedProperty
 
 class GrailsStellaGrailsPlugin {
     // the plugin version
-    def version = "1.0"
+    def version = "1.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1 > *"
     // the other plugins this plugin depends on
