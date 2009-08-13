73496b391ffa4bebc1010d48084e8bc2456fc138
Lucas Teixeira <lucastex@gmail.com>
2009-08-13 17:09:55 -0300

    CPFFormatter TagLib w/ unit test

---
+++ grails-app/taglib/CPFFormatterTagLib.groovy
@@ -0,0 +1,24 @@
+import br.com.caelum.stella.format.CPFFormatter
+import org.codehaus.groovy.grails.web.taglib.exceptions.GrailsTagException
+
+class CPFFormatterTagLib {
+	
+	static namespace = "cpf"
+	
+	def format = { attrs ->
+		def value = attrs.value
+		if (!value)
+			throw new GrailsTagException("Você deve enviar o valor a ser formatado no atributo 'value' da taglib.")
+		
+		return new CPFFormatter().format(value)
+	}
+	
+	def unformat = { attrs ->
+		def value = attrs.value
+		if (!value)
+			throw new GrailsTagException("Você deve enviar o valor a ser formatado no atributo 'value' da taglib.")
+		
+		return new CPFFormatter().unformat(value)
+	}
+
+}
---
+++ test/unit/CPFFormatterTagLibTests.groovy
@@ -0,0 +1,21 @@
+import grails.test.*
+
+class CPFFormatterTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testFormat() {
+		CPFFormatterTagLib taglib = new CPFFormatterTagLib()
+		assertEquals taglib.format(value: "04297060906"), "042.970.609-06"
+    }
+
+    void testUnformat() {
+		CPFFormatterTagLib taglib = new CPFFormatterTagLib()
+		assertEquals taglib.unformat(value: "042.970.609-06"), "04297060906"
+    }
+}
