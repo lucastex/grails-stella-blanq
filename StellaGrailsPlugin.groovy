754c2a0a7bdcdb7a9e9b5898c2f0c68c1c326c3e
Lucas Teixeira <lucastex@macbookpro.local>
2010-04-29 08:08:53 -0300

    Versao 1.3 do plugin

--- StellaGrailsPlugin.groovy
+++ StellaGrailsPlugin.groovy
@@ -3,9 +3,9 @@ import br.com.caelum.grails.stella.constraints.CNPJConstraint
 import br.com.caelum.grails.stella.constraints.NITConstraint
 import org.codehaus.groovy.grails.validation.ConstrainedProperty
 
-class GrailsStellaGrailsPlugin {
+class StellaGrailsPlugin {
     // the plugin version
-    def version =  "1.2.1"
+    def version =  "1.3"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1 > *"
     // the other plugins this plugin depends on
